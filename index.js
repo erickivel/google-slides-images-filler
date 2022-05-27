a6242572cce31344cf415603fe68c55cdcf25dee
Eric Kivel <ericvkivel1@gmail.com>
2022-05-26 23:18:54 -0300

    project setup

---
+++ index.js
@@ -0,0 +1,116 @@
+import fs from 'fs'
+import readline from 'readline'
+import { google } from 'googleapis'
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/presentations'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Slides API.
+  authorize(JSON.parse(content), createImage);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+const pageObjectId = 'id1'
+
+function createImage(auth) {
+  const slides = google.slides({ version: 'v1', auth });
+
+  // 1 mm = 36000 EMU
+  const mm = 36000;
+  const imageId = 'MyImage_01';
+  const emu4M = {
+    magnitude: 90 * mm,
+    unit: 'EMU',
+  };
+  const requests = [{
+    createImage: {
+      objectId: imageId,
+      url: "http://s2.glbimg.com/lTUhRSG_HLiZm_hnV0PtPVRY1dw=/e.glbimg.com/og/ed/f/original/2016/04/05/2c.jpg",
+      elementProperties: {
+        pageObjectId,
+        size: {
+          height: emu4M,
+          width: emu4M,
+        },
+        transform: {
+          scaleX: 1,
+          scaleY: 1,
+          translateX: 100000,
+          translateY: 100000,
+          unit: 'EMU',
+        },
+      },
+    },
+  }];
+
+  slides.presentations.batchUpdate({
+    presentationId: process.env.PRESENTATION_ID,
+    requestBody: {
+      requests,
+    }
+  }, (err, res) => {
+    if (err) {
+      console.log(err);
+
+    } else {
+
+      console.log(`Created image with ID: ${res.data.replies[0].createImage.objectId}`);
+    }
+  })
+}
